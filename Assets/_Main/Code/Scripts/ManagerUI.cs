b09d11759b04c5d4cfff83767aba88b464f5d4da
Alejandro M <midarraga892@soyudemedellin.edu.co>
2025-10-07 08:32:08 -0500

    movimiento arrreglado
    salto corregido

---
+++ Assets/_Main/Code/Scripts/ManagerUI.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerUI : MonoBehaviour
+{
+    [SerializeField]
+    private bool isPaused = false;
+    [SerializeField]
+    private GameObject panel;
+    [SerializeField]
+    private Sprite[] spritesCorazon;
+    [SerializeField]
+    private Image imCorazones;
+    [SerializeField]
+    private Color conllave;
+    [SerializeField]
+    private Color sinllave;
+    [SerializeField]
+    private Image imLlave;
+    void Update()
+    {
+
+
+        // Detectar cuando se presiona la tecla Escape
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        panel.SetActive(true);
+        Time.timeScale = 0f; // Detiene el tiempo del juego
+        isPaused = true;
+
+
+    }
+
+    void ResumeGame()
+    {
+        panel.SetActive(false);
+        Time.timeScale = 1f; // Reanuda el tiempo del juego
+        isPaused = false;
+
+    }
+
+    public void CambiarLlave(bool t)
+    {
+        imLlave.color = t ? conllave : sinllave;
+    }
+
+
+
+    public void ActualizarUIVida(int vidaActual)
+    {
+        if (imCorazones != null && vidaActual >= 0 && vidaActual < spritesCorazon.Length)
+        {
+            imCorazones.sprite = spritesCorazon[vidaActual];
+        }
+        else
+        {
+            Debug.LogError("No esta en el rango o la imagen no esta asiganda");
+        }
+    }
+}
--- Assets/_Main/Code/Scripts/PlayerController.cs
+++ Assets/_Main/Code/Scripts/PlayerController.cs
@@ -1,39 +1,85 @@
 using UnityEngine;
-using Unity.VisualScripting;
+using Unity.VisualScripting;    
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private Rigidbody2D rb2b;
+    private float velocidad = 5f;      // Velocidad horizontal
     [SerializeField]
-    private Vector2 direccion;
+    private float jumpForce = 7f;      // Fuerza del salto
     [SerializeField]
-    private float fuerzadesalto = 100f;
+    private Rigidbody2D rb2d;           // Referencia al Rigidbody2D
     [SerializeField]
-    private float velocidadX;
+    private bool isGrounded = true;   // Para saber si esta en el suelo
+    [SerializeField]
+    private Vector2 v2;
+    [SerializeField]
+    private float estaminaMax = 100f;      // Estamina máxima
+    [SerializeField]
+    private float estaminaActual;          // Estamina actual
+    [SerializeField]
+    private float consumoMovimiento = 10f; // Consumo por moverse (por segundo)
+    [SerializeField]
+    private float consumoSalto = 15f;      // Consumo por salto
+    [SerializeField]
+    private float regeneracion = 20f;      // Regeneración por segundo
+    [SerializeField]
+    private float velocidadReducida = 2.5f;// Velocidad cuando no hay estamina
+    [SerializeField]
+    private bool estaminaCero = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
     void Start()
     {
-        rb2b = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>(); // Busca el Rigidbody2D del jugador
+        estaminaActual = estaminaMax;        // Llena la estamina al inicio
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Movimiento horizontal con derecho/izquierdo
+        float moveInput = Input.GetAxis("Horizontal"); // -1 = izquierda, 1 = derecha mover la entrada 
+        rb2d.linearVelocity = new Vector2(moveInput * velocidad, rb2d.linearVelocity.y);
+
+        float velocidadActual = (estaminaActual > 0) ? velocidad : velocidadReducida;
+        if (Mathf.Abs(moveInput) > 0.01f)
         {
-            rb2b.AddForce(direccion * fuerzadesalto);
+            // Si hay movimiento, consumir estamina
+            estaminaActual -= consumoMovimiento * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else
         {
-            rb2b.AddForce(Vector2.left * velocidadX);
+            // Si no se mueve, regenerar estamina
+            estaminaActual += regeneracion * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+
+        estaminaActual = Mathf.Clamp(estaminaActual, 0, estaminaMax);
+
+
+        rb2d.linearVelocity = new Vector2(moveInput * velocidadActual, rb2d.linearVelocity.y);
+        // Salto con espacio
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb2b.AddForce(Vector2.right * velocidadX);
+            rb2d.AddForce(v2 * jumpForce);
+            //isGrounded = false;
+            estaminaActual -= consumoSalto;
+            estaminaActual = Mathf.Clamp(estaminaActual, 0, estaminaMax);
+            estaminaCero = true;
+
         }
 
     }
-    
+
+    // Detecta si toca el suelo
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+
+        }
+
+    }
+
 }
